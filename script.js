42299a4d40b5ed386c7c6afaf504b3f811380d57
GabrAlmeid <85247121+GabrAlmeid@users.noreply.github.com>
2021-12-23 13:50:57 -0300

    Atributos personalizados

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+let image = document.getElementById("like");
+
+image.addEventListener("click", function() {
+
+   // image.src = "./img/polegarvermelho.png"
+   
+   let lastImage = image.getAttribute("src")
+   image.setAttribute("src", "./img/polegarvermelho.png")
+
+   console.log(lastImage)
+   
+})
+*/
+
+let lista = document.getElementById("lista");
+
+lista.dataset.num = "4"
+
+let id = lista.dataset.id;
+console.log(id)
+
+let num = parseInt(lista.dataset.num);
+console.log(lista.dataset.num)
+
+let conteudo = ""
+
+for(let i = 0; i < num; i++){
+
+    conteudo += "<li>" + i + "</li>"
+
+}
+
+lista.innerHTML = conteudo
